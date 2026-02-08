a44344a1b451c59a6a1321ec51786e0e77cdc17c
plo <leonardopolo266@gmail.com>
2026-02-08 23:53:36 +0100

    implemented kmalloc and expanding heap

--- kernel/include/memory/kheap.h
+++ kernel/include/memory/kheap.h
@@ -2,11 +2,26 @@
 #define KHEAP_H
 
 #include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
 
 // The starting size of our kernel heap (1MB)
 #define KHEAP_STARTING_SIZE 0x100000
+#define KHEAP_MIN_SPLITTING_SIZE 16
+#define KHEAP_BLOCK_SIZE 16
+#define KHEAP_EXTENDING_AMOUNT 0x100000
+
+// The fundamental block of our heap
+struct kheap_node
+{
+    // The size of the region EXCLUDING its header size
+    uint32_t size;
+    bool isFree;
+    struct kheap_node *next;
+};
 
 void kheap_init(void);
+bool kheap_extend(size_t size);
 void* kmalloc(size_t size);
 void kfree(void *ptr);
 
--- kernel/src/memory/kheap.c
+++ kernel/src/memory/kheap.c
@@ -4,14 +4,20 @@
 #include <memory/kheap.h>
 #include <memory/pmm.h>
 #include <memory/vmm.h>
+#include <stddef.h>
 #include <stdint.h>
+#include <stdbool.h>
 
-extern char _KERNEL_END;
-static uint64_t kheap_start;
+// This is a basic implementation of a free list allocator
+
+static uint64_t kheap_start, kheap_end;
+struct kheap_node *kheap_head;
 
 // This function will initialize the kernel heap
 void kheap_init(void)
 {
+    extern char _KERNEL_END;
+
     // We place the kernel heap 1MB after the end of our kernel
     kheap_start = (uint64_t)&_KERNEL_END + 0x100000;
     
@@ -21,25 +27,130 @@ void kheap_init(void)
         kheap_start += VMM_PAGE_SIZE - (kheap_start % VMM_PAGE_SIZE);
     }
 
-    // The total number of pages we need to allocate for our kernel heap
-    uint64_t numPages = KHEAP_STARTING_SIZE / VMM_PAGE_SIZE;
-    if(KHEAP_STARTING_SIZE % VMM_PAGE_SIZE) numPages++;
+    // We set the end at the start since it's empty
+    kheap_end = kheap_start;
+
+    // We map the initial memory region of the heap
+    kheap_extend(KHEAP_STARTING_SIZE);
+
+    log_logLine(LOG_SUCCESS, "%s: Kernel heap initialized\n\tVirtual range: 0x%llx - 0x%llx", __FUNCTION__, kheap_start, kheap_end);
+}
+
+// Extends our kernel heap
+bool kheap_extend(size_t size)
+{
+    // Calculate the number of pages
+    uint64_t numPages = size / VMM_PAGE_SIZE;
+    if(size % VMM_PAGE_SIZE) numPages++;
 
     uint64_t *kernel_pml4_phys = vmm_getKernelRoot();
     
     // Start the mapping
-    uint64_t virtual;
-    for(virtual = kheap_start; virtual < kheap_start + (numPages * VMM_PAGE_SIZE); virtual += VMM_PAGE_SIZE)
+    for(uint64_t virtual = kheap_end; virtual < kheap_end + (numPages * VMM_PAGE_SIZE); virtual += VMM_PAGE_SIZE)
     {
         // Allocate a new page
         uint64_t newPage = pmm_alloc(VMM_PAGE_SIZE);
         if(!newPage)
         {
-            log_logLine(LOG_ERROR, "%s: Cannot allocate memory for kernel heap", __FUNCTION__);
-            hcf();
+            // No more space in the pmm
+            return false;
         }
+        // Map the page
         vmm_map_page(hhdm_physToVirt(kernel_pml4_phys), virtual, newPage, PTE_FLAG_RW | PTE_FLAG_GLOBAL, false);
     }
 
-    log_logLine(LOG_SUCCESS, "%s: Kernel heap initialized\n\tVirtual range: 0x%llx - 0x%llx", __FUNCTION__, kheap_start, virtual);
+    // Get the last node
+    struct kheap_node *current = kheap_head;
+    while(current != NULL && current->next != NULL)
+    {
+        current = current->next;
+    }
+
+    // We have no nodes or the last one is occupied
+    if(current == NULL || current->isFree == false)
+    {
+        struct kheap_node *newNode = (struct kheap_node *) kheap_end;
+        newNode->isFree = true;
+        newNode->size = numPages * VMM_PAGE_SIZE - sizeof(struct kheap_node);
+        newNode->next = NULL;
+
+        if(current == NULL)
+        {
+            // If it's the first node then we set the head
+            kheap_head = newNode;
+        }
+        else 
+        {
+            // Otherwise we attach it to the last one
+            current->next = newNode;
+        }
+    }
+    else // The last node is free so we simply augment it's size
+    {
+        current->size += numPages * VMM_PAGE_SIZE;
+    }
+
+    kheap_end += numPages * VMM_PAGE_SIZE;
+
+    log_logLine(LOG_DEBUG, "%s: The heap has been expanded by %llu bytes; new kheap_end = %llx",__FUNCTION__, numPages * VMM_PAGE_SIZE, kheap_end);
+    return true;
+}
+
+// Our allocating function
+void* kmalloc(size_t size)
+{
+    // Align the size to 16 bytes
+    if(size % KHEAP_BLOCK_SIZE) size += KHEAP_BLOCK_SIZE - (size % KHEAP_BLOCK_SIZE);
+
+    // We iterate through the nodes
+    struct kheap_node *currentNode;
+    while(true)
+    {
+        currentNode = kheap_head;
+
+        // Iterate the list of free nodes
+        while(currentNode != NULL)
+        {
+            // We found a free and big enough block
+            if(currentNode->isFree && currentNode->size >= size)
+            {
+                // If the remaining size is enough for another big enough block we split it
+                if(currentNode->size - size >= sizeof(struct kheap_node) + KHEAP_MIN_SPLITTING_SIZE)
+                {
+                    struct kheap_node *newNode = (struct kheap_node *)((uint8_t *)currentNode + sizeof(struct kheap_node) + size);
+                
+                    // Change the next node for both nodes
+                    newNode->next = currentNode->next;
+                    currentNode->next = newNode;
+
+                    // Set the new node as free and the old one as occupied
+                    newNode->isFree = true;
+                    currentNode->isFree = false;
+
+                    // Set both sizes accordingly
+                    newNode->size = currentNode->size - size - sizeof(struct kheap_node);
+                    currentNode->size = size;
+
+                    // Return the usable address
+                    return (void *)(currentNode + 1); 
+                }
+                else 
+                {
+                    // Else we just allocate it 
+                    currentNode->isFree = false;
+                    return (void *)(currentNode + 1);
+                }
+            }
+
+            // We go to the next node
+            currentNode = currentNode->next;
+        }
+
+        // If we're here it's because we didn't find a big enough block
+        if(!kheap_extend(KHEAP_EXTENDING_AMOUNT))
+        {
+            // The heap expansion has failed
+            return NULL;
+        }
+    }
 }
